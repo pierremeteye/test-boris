228220f362adf66d6e88c11976cd5492c81132c5
DESKTOP-8UQVMR5\admin <pmeteye@hitpart.fr>
2018-06-29 12:31:10 +0200

    live commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Enzyme, { shallow, render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { createSerializer } from "enzyme-to-json";
+import sinon from "sinon";
+
+import Form from "./Components/Form";
+
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+
+it("renders correctly", () => {
+  const wrapper = shallow(
+    <Form />
+  );
+
+  expect(wrapper).toMatchSnapshot();
 });
